64eaf50a7774b7845745a311bfef4cbbbb13e42e
ungvietanh20172394 <ungvietanh120799@gmail.com>
2021-05-06 23:30:26 +0700

    update gui

---
+++ app/src/main/java/vn/vietanh/quanlychitieu/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package vn.vietanh.quanlychitieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import vn.vietanh.quanlychitieu.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+            }
+        },1500);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+}
---
+++ app/src/main/java/vn/vietanh/quanlychitieu/fragment/ThongKeFragment.java
@@ -0,0 +1,49 @@
+package vn.vietanh.quanlychitieu.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.vietanh.quanlychitieu.R;
+import vn.vietanh.quanlychitieu.adapter.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+public class ThongKeFragment extends Fragment {
+    View view;
+    ViewPager vpThongKe;
+    TabLayout tlThongKe;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_thong_ke, container, false);
+        initView();
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        addTabs(vpThongKe);
+        tlThongKe.setupWithViewPager(vpThongKe);
+    }
+
+    public void initView(){
+        vpThongKe = view.findViewById(R.id.vpThongke);
+        tlThongKe =  view.findViewById(R.id.tlThongke);
+    }
+    public void addTabs(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFrag(new TKHomNayFragment(),"Hôm nay");
+        adapter.addFrag(new TKThangFragment(),"Tháng");
+        adapter.addFrag(new TKNamFragment(),"Năm");
+        viewPager.setAdapter(adapter);
+    }
+}
